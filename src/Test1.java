ffa870b1c28e7310c33d667af0762b21f8d36c4c
bohyeonj <89669608+bohyeonj@users.noreply.github.com>
2021-11-05 20:44:55 -0400

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,24 +1,21 @@
 public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
 
-        switch(operation) {
-            case ADD:
+        if(operation == ADD) {
                 System.out.println("CS193 sucks");
                 break;
-            case SUBTRACT:
+        } else if (operation == SUBTRACT) {
                 System.out.println("CS193 is not everyone's favorite class");
                 break;
-            case MULTIPLY:
-                System.out.println("The CS193 lecturers do not want us to thrive in CS");
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
+        } else if (operation == MULTIPLY) {
+                  System.out.println("The CS193 lecturers do not want us to thrive in CS");
+        } else if (operation == DIVIDE) {
+                 if (num2 == 0) {
                     System.out.println("Error: u suck");
                 } else {
                     result = num1 / num2;
                 }
                 break;
         }
-
-        return result;
+      return result;
     }
